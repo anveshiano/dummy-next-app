39f9525ac7694efad1ad136335301f0708ab2e4c
anveshisky <anveshi.manne@sky.uk>
2023-08-23 15:01:47 +0100

    JGIT-5 git-jira integration test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,10 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Get started by editing:
+        
         </p>
+        <p>Task 5</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
